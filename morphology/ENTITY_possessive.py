c1244afee99e23cefa852399353ce013acc3092f
Peter <peter.w@droidtown.co>
2023-10-18 00:47:23 +0800

    add two basic lexical dictionaries.

---
+++ morphology/ENTITY_possessive.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Developer: Peter. w (peter.w@droidtown.co)
+
+EastPaiwan = {"ku"       : "我的",
+              "niaken"   : "我的",
+              "nisun"    : "你的",
+              "nimadju"  : "他的",
+              "nimitja"  : "我們的",
+              "nimun"    : "你們的",
+              "niamadju" : "他們的",
+              "tia"      : "咱們的"
+              }
+
+SouthPaiwan = {"niaken"   : "我的",
+               "nisun"    : "你的",
+               "nimadju"  : "他的",
+               "niamen"   : "我們的",
+               "nimun"    : "你們的",
+               "niamadju" : "他們的",
+               "nimitja"  : "咱們的"
+               }
+
+MiddlePaiwan = {"ni a ken"   : "我的",
+                "ni sun"     : "你的",
+                "ni madju"   : "他的",
+                "ni tjen"    : "我們的",
+                "ni mun"     : "你們的",
+                "ni a madju" : "他們的",
+                "ni mitja"   : "咱們的"
+                }
+
+NorthPaiwan = {"niaken"   : "我的",
+               "nisun"    : "你",
+               "nimadju"  : "他的",
+               "niamen"   : "我們的",
+               "nimun"    : "你們的",
+               "niamadju" : "他們的",
+               "ntjen"    : "咱們的"
+               }
---
+++ morphology/ENTITY_pronoun.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Developer: Peter. w (peter.w@droidtown.co)
+
+EastPaiwan = {"tiaken"  : "我",
+              "aken"    : "我",
+              "tisun"   : "你",
+              "sun"     : "你",
+              "timadju" : "他",
+              "timitja" : "我們",
+              "timun"   : "你們",
+              "tiamadju": "他們",
+              "tiamen"  : "我們",
+              "tja"     : "咱們",
+              }
+
+SouthPaiwan = {"tiaken"         : "我",
+               "tisun"          : "你",
+               "timadju"        : "他",
+               "tiamen"         : "我們",
+               "timun"          : "你們",
+               "tiamadju"       : "他們",
+               "nimitja tiamen" : "我們",
+               "niamen"         : "我們",
+               "timitja"        : "咱們",
+               "nimitja"        : "咱們"
+               }
+
+MiddlePaiwan = {"ti a ken"   : "我",
+                "ti sun"     : "你",
+                "ti madju"   : "他",
+                "ti a madju" : "我們",
+                "ti mun"     : "你們",
+                "titjen"     : "他們",
+                "ti a men"   : "我們",
+                "ti tjen"    : "咱們",
+                "ti mitja"   : "咱們"
+                }
+
+NorthPaiwan = {"tiaken"   : "我",
+               "tisun"    : "你",
+               "timadju"  : "他",
+               "tiamen"   : "我們",
+               "timun"    : "你們",
+               "tiamadju" : "他們",
+               "titjen"   : "咱們"
+               }
